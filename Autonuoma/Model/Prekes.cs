39d7256f760a25fe9dd8a2c88c7ad408e717e805
rms-ctrl <domas.ald@gmail.com>
2022-12-06 21:03:20 +0200

    Added new models

---
+++ Autonuoma/Model/Prekes.cs
@@ -0,0 +1,20 @@
+﻿namespace Autonuoma.Model
+{
+    public class Prekes
+    {
+        public string Pavadinimas { get; set; }
+        public double Kaina { get; set; }
+        public string Aprasymas { get; set; }
+        public int PatinkaPaspaudimai { get; set; }
+        public string NuotraukaLink { get; set; }
+        public string Spalva { get; set; }
+        public string Medziaga { get; set; }
+        public string Matmenys { get; set; }
+        public string Svoris { get; set; }
+        public int GarantijosTrukme { get; set; }
+        public int Dydis { get; set; }
+        public int Id { get; set; }
+        public string fk_Kategorija { get; set; }
+        public int fk_GamybosVieta { get; set; }
+    }
+}
